2c3fd0b41b6bc8f60a2c907ac9fac09e3c29ca7e
pkorver <pkorver@tmns.com>
2018-08-01 15:07:14 +0200

    now creates the outer boxes of the grid in accordance to the set size

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inputElements = {
     createGrid : document.getElementById('create_grid'),
+    gridContainer : document.querySelector('.container'),
 	inputWidth : document.getElementById('width'),
 	inputLength : document.getElementById('length')
 };
@@ -8,7 +9,46 @@ const inputElements = {
 
 inputElements.createGrid.addEventListener('click', e => {
 	e.preventDefault();
-	const width = parseInt(inputElements.inputWidth.value);
-	const length = parseInt(inputElements.inputLength.value);
+	const gridWidth = parseInt(inputElements.inputWidth.value);
+	const gridLength = parseInt(inputElements.inputLength.value);
 	console.log(width);
+	inputElements.gridContainer.innerHTML = '';
+	for(var i = 0; i < gridLength; i++){
+		for(var j = 0; j < gridWidth; j++){
+			if(j === 0 && i === 0){
+				inputElements.gridContainer.insertAdjacentHTML('beforeend',
+				`<div class="no_border_square_top">
+    				<div class="corner">
+    				</div>
+    			</div>`);
+    		} else if(i === 0){
+    			inputElements.gridContainer.insertAdjacentHTML('beforeend',
+    			`<div class="no_border_square_top">
+    				<div class="numTents">
+    					<p class="top_num_tents" ><input type="number"
+       						min="0"
+       						max="2"
+       						step="1"
+       						value="0"/>
+       					</p>
+    				</div>
+    			</div>`);
+    		} else if(j === 0 && i !== 0){
+    			inputElements.gridContainer.insertAdjacentHTML('beforeend',
+    				`<div class="no_border_square_front">
+    					<div class="numTents">
+    						<p class="front_num_tents"><input type="number"
+       							min="0"
+       							max="2"
+       							step="1"
+       							value="0"/>
+       						</p>
+    					</div>
+    				</div>`);
+    		}
+
+
+		}
+	}
+
 });
