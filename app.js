68fa344daae52b61b3ad85c9795567e74aa2c285
pkorver <pkorver@tmns.com>
2018-08-28 15:48:56 +0200

    Making progress on the right sizing of the squaer, works for 4 till 7 now.

--- app.js
+++ app.js
@@ -12,25 +12,41 @@ inputElements.createGrid.addEventListener('click', e => {
 	const gridWidth = parseInt(inputElements.inputWidth.value);
 	const gridLength = parseInt(inputElements.inputLength.value);
 	console.log(width);
-  const squareSize = (70 / gridWidth);
+  const squareSize = (90 / (gridWidth + 1));
   console.log(squareSize);
 
 	inputElements.gridContainer.innerHTML = '';
 	for(var i = 0; i < gridLength+1; i++){
 		for(var j = 0; j < gridWidth+1; j++){
 			if(j === 0 && i === 0){
+        console.log("adding the empty start");
 				inputElements.gridContainer.insertAdjacentHTML('beforeend',
 				`<div class="no_border_square_top" style="width:${squareSize}%;
-    padding-bottom:${squareSize}%">
+          padding-bottom:${squareSize}%">
     				<div class="corner">
     				</div>
     			</div>`);
-    		} else if(i === 0){
+    		} else if(j === 0 && i !== 0){
+          console.log("adding a front num box");
+          inputElements.gridContainer.insertAdjacentHTML('beforeend',
+            `<div class="no_border_square_front" style="width:${squareSize}%;
+              padding-bottom:${squareSize}%">
+              <div class="numTents">
+                <p class="front_num_tents"><input  style="width:80%;" type="number"
+                    min="0"
+                    max="2"
+                    step="1"
+                    value="0"/>
+                  </p>
+              </div>
+            </div>`);
+        } else if(i === 0){
+          console.log("adding a top num box");
     			inputElements.gridContainer.insertAdjacentHTML('beforeend',
     			`<div class="no_border_square_top" style="width:${squareSize}%;
-    padding-bottom:${squareSize}%">
+            padding-bottom:${squareSize}%">
     				<div class="numTents">
-    					<p class="top_num_tents" ><input type="number"
+    					<p class="top_num_tents" ><input  style="width:80%;" type="number"
        						min="0"
        						max="2"
        						step="1"
@@ -38,23 +54,11 @@ inputElements.createGrid.addEventListener('click', e => {
        					</p>
     				</div>
     			</div>`);
-    		} else if(j === 0 && i !== 0){
-    			inputElements.gridContainer.insertAdjacentHTML('beforeend',
-    				`<div class="no_border_square_front" style="width:${squareSize}%;
-    padding-bottom:${squareSize}%">
-    					<div class="numTents">
-    						<p class="front_num_tents"><input type="number"
-       							min="0"
-       							max="2"
-       							step="1"
-       							value="0"/>
-       						</p>
-    					</div>
-    				</div>`);
     		} else {
+          console.log("adding a square box");
           inputElements.gridContainer.insertAdjacentHTML('beforeend',
             `<div class="square" style="width:${squareSize}%;
-    padding-bottom:${squareSize}%">
+              padding-bottom:${squareSize}%">
               <div class="content">
               </div>
             </div>`);
